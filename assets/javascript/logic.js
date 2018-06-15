64eab963644d9cd2802dee75da1226884dbd327c
Evan <ewilsons@aol.com>
2018-06-14 23:45:46 -0400

    first draft

---
+++ assets/javascript/logic.js
@@ -0,0 +1,77 @@
+// create array called topics with all my fave stuff, display them as individual buttons in #button-area div
+// get user input, that that appends new button on click. use push method to add to the array
+// set up click event that grabs the buttons and calls ajax request to giphy
+// will need a for loop and var i to creat uniqu identifiers
+
+var topics = ["cars", "video games", "guitar", "food", "spiders", "roller coasters"]
+var newTopic = "";
+var apiKey = "33dbXZtSVdT9qRXABAxgAN2FtGTormNK";
+var searchWord;
+var queryUrl = "http://api.giphy.com/v1/gifs/search?q=guitar&api_key=33dbXZtSVdT9qRXABAxgAN2FtGTormNK&limit=9";
+
+$("#submit").click(function () {
+    event.preventDefault();
+    newGif = $("#user-input").val().trim();
+    newGifBtn = $("<button class='btn btn-info btn-sm'>");
+    newGifBtn.text(newGif)
+    topics.push(newGif);
+    $("#button-area").append(newGifBtn)
+    console.log(newGif, topics)
+})
+
+function getTheGif() {
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log("----------------------")
+
+            var gifURL = response.data.images.url;
+            var gifImg = $("<img>");
+            $("#gif-area").append(gifImg);
+            console.log(gifURL);
+
+        });
+}
+function makeBtns() {
+
+
+    for (var i = 0; i < topics.length; i++) {
+
+
+
+        var topicButton = $("<button>");
+        topicButton.attr("id", "topicBtn-" + i);
+        topicButton.attr("class", "btn btn-info btn-sm m-1");
+        topicButton.text(topics[i]);
+        $("#button-area").append(topicButton);
+        searchWord = topics[i];
+        console.log(searchWord)
+
+
+    }
+}
+
+
+$(document).on("click", ".btn", getTheGif)
+console.log(queryUrl);
+getTheGif();
+makeBtns();
+
+
+
+    // $(document).on("click", "#submit", (function addNewTopic() {
+    //     event.preventDefault();
+
+    //     var newTopicText = $("#user-input").val().trim();
+    //     topics.push(newTopicText);
+    //     newTopic = $("<button>");
+    //     newTopic.attr("class", "btn btn-info btn-sm m-1")
+    //     newTopic.text(newTopicText);
+
+    //     $("#button-area").append(newTopic);
+    //     $("#user-input").empty()
+    //     console.log(topics);
+    // }));
